ea24172867b8db2404d39c65245328fde0746801
nicotina04 <devherdson@gmail.com>
2025-05-11 22:34:58 +0900

    refactor: split contest status entity feature creation feature

--- cf_data_pipeline/contest_standing_fetcher.py
+++ cf_data_pipeline/contest_standing_fetcher.py
@@ -12,56 +12,74 @@ def is_provisional_or_unrated_handle(handle: str) -> bool:
         return None, None
     return len(res['result']) < 5, len(res['result']) == 0
 
+def get_entity_from_rating_change(items: list[dict]) -> dict:
+    ratings = list()
+    rating_sum_all = 0
+    unrated_count = 0
+    for item in items:
+        old_rating = item['oldRating']
+        rating_sum_all += old_rating
+        if old_rating != 0:
+            ratings.append(old_rating)
+        else:
+            unrated_count += 1
+    avg_rating_all = rating_sum_all // max(1, len(items))
+    avg_rated_only = rating_sum_all // max(1, len(items) - unrated_count)
+    ratings.sort()
+    sum_of_variance = 0
+    for rating in ratings:
+        if rating is not None:
+            sum_of_variance += (rating - avg_rated_only) ** 2
+    std = (sum_of_variance / max((len(items) - unrated_count), 1)) ** 0.5
+
+    entity = {
+        'avg_rating_all': avg_rating_all,
+        'avg_rating_rated_only': avg_rated_only,
+        'median_rating_rated': ratings[len(ratings) // 2] if len(ratings) > 0 else 0,
+        '25th_percentile_rated': ratings[int(len(ratings) * 0.25)] if len(ratings) > 0 else 0,
+        '75th_percentile_rated': ratings[int(len(ratings) * 0.75)] if len(ratings) > 0 else 0,
+        'std_rating_rated': std,
+        'count_total': len(items),
+        'count_unrated': unrated_count,
+        'unrated_ratio': round(unrated_count / max(1, len(items)), 3),
+    }
+    return entity
+
+
 def process_contest_standings():
     rated_contests = get_rated_contest_df()
     contests = rated_contests['contest_id'].tolist()
     entity_list = list()
+    failed_cid = list()
+    total_failed = list()
 
     for contest_id in contests:
         res_rating_changes = api_client.get_contest_rating_changes(contest_id)
         time.sleep(SLEEP_TIME)
+        if res_rating_changes is None:
+            failed_cid.append(contest_id)
+            continue
         items = res_rating_changes['result']
-
-        ratings = list()
-        total_user_count = len(items)
-        unrated_count = 0
-        rating_sum_all = 0
-        rating_sum_rated = 0 
-
-        for item in items:
-            old_rating = item['oldRating']
-            rating_sum_all += old_rating
-            if old_rating == 0:
-                unrated_count += 1
-            else:
-                ratings.append(old_rating)
-                rating_sum_rated += old_rating
+        entity = get_entity_from_rating_change(items)
+        print(f"[INFO] {contest_id} - {entity}")
+        entity_list.append(entity)
     
-        avg_rating_all = rating_sum_all // total_user_count
-        avg_rated_only = rating_sum_all // (total_user_count - unrated_count)
-        ratings.sort()
-        sum_of_variance = 0
-        for rating in ratings:
-            if rating is not None:
-                sum_of_variance += (rating - avg_rated_only) ** 2
-        std = (sum_of_variance / max((total_user_count - unrated_count), 1)) ** 0.5
-        
-        entity = {
-            'contest_id': contest_id,
-            'avg_rating_all': avg_rating_all,
-            'avg_rating_rated_only': avg_rated_only,
-            'median_rating_rated': ratings[len(ratings) // 2] if len(ratings) > 0 else 0,
-            'std_rating_rated': std,
-            'count_total': total_user_count,
-            'count_unrated': unrated_count,
-            'unrated_ratio': unrated_count / total_user_count,
-        }
+    for contest_id in failed_cid:
+        res_rating_changes = api_client.get_contest_rating_changes(contest_id)
+        if res_rating_changes is None:
+            total_failed.append(contest_id)
+            continue
+
+        time.sleep(SLEEP_TIME)
+        items = res_rating_changes['result']
+        entity = get_entity_from_rating_change(items)
         print(f"[INFO] {contest_id} - {entity}")
         entity_list.append(entity)
 
     df = pd.DataFrame(entity_list)
     contest_data_path = PROCESSED_DATA_DIR / f'contest_statistics.csv'
     storage.save_csv(contest_data_path, df)
+    print(f'Failed contests: {total_failed}')
 
 def process_user_result():
     rated_contests = get_rated_contest_df()
