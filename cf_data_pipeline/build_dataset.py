9d19cf2533afd7b46f5416821d5c2fe86dea1a3b
nicotina04 <devherdson@gmail.com>
2025-05-13 14:01:43 +0900

    feat: add parsing contest logic

--- cf_data_pipeline/build_dataset.py
+++ cf_data_pipeline/build_dataset.py
@@ -1,5 +1,21 @@
 import sqlite3
+from typing import Optional
+import storage
+import config
 import db_contest_user_result
+
+
+def get_contest_statistics(contest_id: int) -> Optional[dict]:
+    df_contest_statistics = storage.load_csv(config.CONTEST_STATISTICS_PATH)
+    row = df_contest_statistics[df_contest_statistics['contest_id'] == contest_id]
+    if not row.empty:
+        return row.iloc[0].to_dict()
+    else:
+        return None
+
+
+def get_problem_info(contest_id: int, problem_idx: int) -> Optional[dict]:
+    pass
 
 
 def create_dataset():
@@ -17,4 +33,4 @@ def create_dataset():
             problem_index_num = record[2]
             problem_index_raw = record[3]
             verdict = record[4]
-            record.get('contest_id')
+
