c7ff9b9f84d73da8248b545f35a53507a2368f40
nicotina04 <devherdson@gmail.com>
2025-06-18 00:07:06 +0900

    fix: change feature name to f%d

--- model_export.py
+++ model_export.py
@@ -1,11 +1,16 @@
 import joblib
-import onnxmltools
 import numpy as np
+import onnxmltools
+from onnxmltools.convert.common.data_types import FloatTensorType
+from xgboost import XGBClassifier
 
 
 if __name__ == '__main__':
-    model = joblib.load('models/XGBoost.pkl')
-    dummy_input = np.random.rand(1, model.n_features_in_)
-    types = [('input', onnxmltools.convert.common.data_types.FloatTensorType([None, model.n_features_in_]))]
-    onnx_model = onnxmltools.convert_xgboost(model, initial_types=types)
+    model: XGBClassifier = joblib.load('models/XGBoost.pkl')
+    n_features = model.n_features_in_
+
+    model.get_booster().feature_names = [f'f{i}' for i in range(n_features)]
+    dummy_input = np.random.rand(1, n_features).astype(np.float32)
+    input_type = [('input', FloatTensorType([None, n_features]))]
+    onnx_model = onnxmltools.convert_xgboost(model, initial_types=input_type)
     onnxmltools.utils.save_model(onnx_model, 'models/XGBoost.onnx')
